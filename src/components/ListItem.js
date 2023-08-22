c6a3eb46ee6567b223628f0bc5eb9475e79c931e
Zhi Kang Xie <63524324+zhikangxie107@users.noreply.github.com>
2023-08-22 15:08:30 -0600

    Removed Console Log in ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,7 +7,7 @@ import moment from 'moment'
 const ListItem = (props) => {
   const { dt_txt, min, max, condition } = props
   const { item, date, temp, dateTextWrapper } = styles
-  console.log(condition)
+  
   return (
     <View style={item}>
       <Feather name={weatherType[condition].icon} size={50} color={'white'} />
